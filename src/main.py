9bc6c586dadc2f8bdd1f0a6616c5565a1f3a10f8
rhenzo <rhenzo@rhenzoDebian.168.0.119>
2024-12-02 09:14:02 -0300

    Creating a notebook

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split 
 
 # Caminho para o dataset
 file_path = "data/AmesHousing.csv"
@@ -6,5 +9,11 @@ file_path = "data/AmesHousing.csv"
 # Carregar os dados
 data = pd.read_csv(file_path)
 
+# Escolhendo qual é o alvo
+target = data.SalePrice
+
+# RandomForest
+random_forest_model = RandomForestRegressor(random_state=1)
+
 # Exibir as primeiras linhas
-print(data.head())
+print(data.head())  
